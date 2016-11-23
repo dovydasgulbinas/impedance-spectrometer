225ce1367188d670ebfc45e50d7a3bd8cf1be1d3
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-11-23 16:16:24 +0000

    adding comments on how to attach a method

--- ion_spectroscope/main.py
+++ ion_spectroscope/main.py
@@ -21,11 +21,11 @@ class DataSignals(QtCore.QObject):
     # Python specific type -- list
     value_updated = QtCore.pyqtSignal(list)
 
-    def value_generator(self):
+    def value_generator(self, value):
         # will generate event that will passed
         # it should be replace with real data stream
         # value emiter is crucial part of the code
-        self.value_updated.emit([random.random(),random.random()]*5)
+        self.value_updated.emit(value)
 
 
 class MainWindow(QtGui.QMainWindow, spectro_gui.Ui_MainWindow):
@@ -39,6 +39,7 @@ class MainWindow(QtGui.QMainWindow, spectro_gui.Ui_MainWindow):
         # -- Data refresh configuration
         # Must use self!
         self.data_signals = DataSignals(self)
+        # this method will get a emited signal value 
         self.data_signals.value_updated.connect(self.handle_value_updated)
 
     def customize_left_plot(self):
@@ -52,15 +53,8 @@ class MainWindow(QtGui.QMainWindow, spectro_gui.Ui_MainWindow):
         self.graph_l0.setData(value)
         print(value)
         # essential line for updating
-        QtGui.qApp.processEvents() # force complete redraw for every plot
+        QtGui.qApp.processEvents() # force complete redraw for every lot
 
-    def temp_plot_always(self):
-        # plot method takes in lists!
-        self.graph_l0.setData(value)
-        print(value)
-        self.plot_left.setTitle(value)
-        # essential line for updating
-        QtGui.qApp.processEvents() # force complete redraw for every plot
 
 
 if __name__ == "__main__":
@@ -68,10 +62,8 @@ if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    # Implementing timed data polling
-    timer = QtCore.QTimer()
-    timer.timeout.connect(window.data_signals.value_generator)
-    timer.start(20)
+    # I must call value generator method and pass data to it  
+    window.data_signals.value_generator()
     # this line is not executed
     sys.exit(app.exec_())
 
