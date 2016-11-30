9456ad4d4b5657bcd119278a8c0e751c521e2f88
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-11-30 13:25:19 +0200

    adding data based hander

--- ion_spectroscope/main.py
+++ ion_spectroscope/main.py
@@ -1,5 +1,9 @@
+import logging
 from PyQt4 import QtCore, QtGui
-from Ui_MainWindow import spectro_gui 
+from Ui_MainWindow import spectro_gui
+from .tiepie_hs5 import SpectroscopeManager
+
+logger = logging.getLogger()
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -9,6 +13,8 @@ except AttributeError:
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
+
+
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
@@ -30,21 +36,32 @@ class MainWindow(QtGui.QMainWindow, spectro_gui.Ui_MainWindow):
         # this returns one instance of a graph plotted in left_plot
         self.graph_l0 = self.plot_left.plot()
 
-    def handle_value_updated(self, value): # you must pass value
+    def handle_value_updated(self, value):  # you must pass value
         # plot method takes in lists!
-        self.graph_l0.setData(value)
+        # fixme: a hardcoded value index should be removed
+        self.graph_l0.setData(value[0])
         print(value)
         # essential line for updating
-        QtGui.qApp.processEvents() # force complete redraw for every plot
+        QtGui.qApp.processEvents()  # force complete redraw for every plot
 
 
 if __name__ == "__main__":
     import sys
+
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    logger.debug('Running as main')
+
     app = QtGui.QApplication(sys.argv)
     window = MainWindow()
     window.show()
 
+    # adding osciloscope instance
+
+    scopes = SpectroscopeManager()
+    # attaching a handler
+    scopes.set_new_data_handler(window.handle_value_updated)
+    scopes.setup_block_measurment(1000, 100, [[0, 8]], info=True)
+    scopes.do_block_measurment(400)
 
     # this line is not executed
     sys.exit(app.exec_())
-
